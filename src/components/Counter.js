d748f2a40949c953f2a97d596d3860829a78b3e4
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-02-23 11:50:21 +0530

    create a function for buttons

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,12 +2,27 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [counter, setcounter] = useState(0);
+  function increment() {
+    console.log("count increment");
+  }
+   function decrement() {
+     console.log("count decrement");
+   }
+    function reset() {
+      console.log("count reset");
+    }
   return (
     <>
       <h1>Counter : {counter}</h1>
-      <button style={{ margin: "0 5px" }}>Increment</button>
-      <button style={{ margin: "0 5px" }}>Decrement</button>
-      <button style={{ margin: "0 5px" }}>Reset</button>
+      <button onClick={() => increment()} style={{ margin: "0 5px" }}>
+        Increment
+      </button>
+      <button onClick={() => decrement()} style={{ margin: "0 5px" }}>
+        Decrement
+      </button>
+      <button onClick={() => reset()} style={{ margin: "0 5px" }}>
+        Reset
+      </button>
     </>
   );
 };
