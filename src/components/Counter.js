6c97ab5a03ed57062984d48331d8e30aeb69e3a5
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-02-23 11:47:18 +0530

    create 3 button

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Counter = () => {
+  const [counter, setcounter] = useState(0);
   return (
-    <h1>hello</h1>
-  )
-}
+    <>
+      <h1>Counter : {counter}</h1>
+      <button style={{ margin: "0 5px" }}>Increment</button>
+      <button style={{ margin: "0 5px" }}>Decrement</button>
+      <button style={{ margin: "0 5px" }}>Reset</button>
+    </>
+  );
+};
 
-export default Counter
+export default Counter;
